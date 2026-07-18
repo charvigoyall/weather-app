d5a034b9313eb12dd2b107a8275534d2af86ca9f
charvigoyall <charvigoyal444@gmail.com>
2026-07-18 23:55:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showWeather(position) {
 
     // Fetch weather data
     //weather file
-    /*comment */
+   
     fetch(weatherURL)
         .then(response => response.json())
         .then(data => {
